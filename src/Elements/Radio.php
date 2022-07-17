992820639f683d35890adc5ccf2937058304fd09
Roel Magdaleno <roelmagdaleno@gmail.com>
2022-07-17 12:45:15 -0500

    FEATURE: Add Radio input element

---
+++ src/Elements/Radio.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Roel\WPFormElements\Elements;
+
+use Roel\WPFormElements\Element;
+
+class Radio extends Element {
+	/**
+	 * Render the HTML component.
+	 *
+	 * @since  0.1.0
+	 *
+	 * @return string   The HTML component.
+	 */
+	public function render() : string {
+		$html = '<fieldset>';
+
+		foreach ( $this->settings['options'] as $value => $option ) {
+			$html .= '<p> <label>';
+			$html .= '<input type="radio" id="' . esc_attr( $value ) . '" ';
+			$html .= 'name="' . esc_attr( $this->name() ) . '" value="' . esc_attr( $value ) . '" ';
+			$html .= checked( $value, $this->value(), false ) . '/>' . $option['label'];
+			$html .= '</p> </label>';
+		}
+
+		$html .= $this->description();
+		$html .= '</fieldset>';
+
+		return $html;
+	}
+}
